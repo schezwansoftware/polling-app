28f5d0a837bfbd46f176abd6dbe54ff541189bb8
harshit <harshit.hartron@gmail.com>
2018-08-06 12:20:36 +0530

    first commit

---
+++ src/main/java/com/condescendors/pollingapp/pollingapp/PollingappApplication.java
@@ -0,0 +1,12 @@
+package com.condescendors.pollingapp.pollingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingappApplication.class, args);
+	}
+}
