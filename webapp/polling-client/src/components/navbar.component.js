42991fb4abbe0ef4c2cdc7ae6c69d16fcb69ff32
Harshit <harshit.hartron@gmail.com>
2018-08-23 12:36:40 +0530

    component added

---
+++ webapp/polling-client/src/components/navbar.component.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Layout, Menu ,Icon} from 'antd';
+
+const { Header } = Layout;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const NavBar=()=>{
+
+    return(
+<Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="light"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">About</Menu.Item>
+        <Menu.Item key="3">Contact Us</Menu.Item>
+       
+       <SubMenu title={<span><Icon type="setting" />Account</span>}>
+            <Menu.Item key="setting:1">Sign In</Menu.Item>
+            <Menu.Item key="setting:2">Register</Menu.Item>
+        </SubMenu>
+
+      </Menu>
+    </Header>
+ </Layout>
+    );
+}
+
+export default NavBar;
