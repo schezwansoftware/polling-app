fe3bb12cb1e81a073f07e0f6445bcf324bea3fe7
Harshit <harshit.hartron@gmail.com>
2018-08-23 15:31:57 +0530

    login component created

--- webapp/polling-client/src/components/login.component.js
+++ webapp/polling-client/src/components/login.component.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React,{Component} from 'react';
+import { Form, Icon, Input, Button, Checkbox, Card, Modal} from 'antd';
+import './styles/css/login.css';
+import {Link} from 'react-router-dom';
 
-const Login= () => {
 
-    return(
-        <h1>Login Component</h1>
+const FormItem =Form.Item;
+const Login=()=> {
+    const LoginForm=Form.create()(AntWrappedLoginForm);
+    return (
+   <div className="login-container">
+     <Card title="Log In">
+        <div className="logn-content">
+           <LoginForm />
+        </div>
+     </Card>
+    </div>
     );
 }
 
+
+class AntWrappedLoginForm extends Component{
+   
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+            Modal.success({
+
+                title:'LogIn Success',
+                content: 'You have logged in Successfully'
+            });
+          }
+        });
+    }
+   
+    render(){
+        const {getFieldDecorator}=this.props.form;
+        return(
+            <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input size="large" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input size="large" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a className="login-form-forgot" href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" size="large" className="login-form-button">
+            Log in
+          </Button>
+          Or <Link to="/register">register now!</Link>
+        </FormItem>
+      </Form>
+        );
+    }
+}
 export default Login;
--- webapp/polling-client/src/components/navbar.component.js
+++ webapp/polling-client/src/components/navbar.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{Component} from "react";
 
 import { Layout, Menu ,Icon} from 'antd';
 import {NavLink} from 'react-router-dom';
@@ -6,31 +6,49 @@ const { Header } = Layout;
 const SubMenu = Menu.SubMenu;
 
 
-const NavBar=() => {
+class NavBar extends Component {
 
+  constructor(props){
+    super(props);
+    this.state={
+      current: '1'
+    }
+
+    this.handleMenuClick=this.handleMenuClick.bind(this);
+  }
+
+  handleMenuClick(e){
+    this.setState({
+      current : e.key  
+    })
+  }
+
+  render(){
     return(
-<Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="light"
-        mode="horizontal"
-        defaultSelectedKeys={['1']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1"><NavLink to="/">Home</NavLink></Menu.Item>
-        <Menu.Item key="2"><NavLink to="/about">About Us</NavLink></Menu.Item>
-        <Menu.Item key="3"><NavLink to="/contact">Contact Us</NavLink></Menu.Item>
-       
-       <SubMenu title={<span><Icon type="setting" />Account</span>}>
-            <Menu.Item key="setting:1"><NavLink to="/login">Sign In</NavLink></Menu.Item>
-            <Menu.Item key="setting:2"><NavLink to="/register">Register</NavLink></Menu.Item>
-        </SubMenu>
-
-      </Menu>
-    </Header>
- </Layout>
-    );
+      <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="light"
+              mode="horizontal"
+              style={{ lineHeight: '64px' }}
+              selectedKeys={[this.state.current]}
+              onClick={this.handleMenuClick}
+            >
+              <Menu.Item key="1"><NavLink to="/">Home</NavLink></Menu.Item>
+              <Menu.Item key="2"><NavLink to="/about">About Us</NavLink></Menu.Item>
+              <Menu.Item key="3"><NavLink to="/contact">Contact Us</NavLink></Menu.Item>
+             
+             <SubMenu key="4" title={<span><Icon type="setting" />Account</span>}>
+                  <Menu.Item key="setting:1"><NavLink to="/login">Sign In</NavLink></Menu.Item>
+                  <Menu.Item key="setting:2"><NavLink to="/register">Register</NavLink></Menu.Item>
+              </SubMenu>
+      
+            </Menu>
+          </Header>
+       </Layout>
+          );
+  }
 }
 
 export default NavBar;
